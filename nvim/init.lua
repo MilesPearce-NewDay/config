9afed1fd01a6cbe2be21808a7aeb89c636156307
Miles Pearce <n45731@newday.co.uk>
2025-12-12 11:54:51 +0000

    Add config files

---
+++ nvim/init.lua
@@ -0,0 +1,152 @@
+-- ============================================================
+--  PACKER INITIALIZATION
+-- ============================================================
+
+-- Auto-install packer if not installed
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+-- Automatically run :PackerCompile whenever this file is saved
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost init.lua source <afile> | PackerCompile
+  augroup end
+]])
+
+-- ============================================================
+--  GENERAL OPTIONS
+-- ============================================================
+vim.g.mapleader = " "
+vim.o.termguicolors = true
+vim.o.number = true
+vim.o.relativenumber = true
+-- Use spaces instead of tabs
+vim.opt.expandtab = true
+
+-- Number of spaces that a <Tab> counts for
+vim.opt.tabstop = 4
+
+-- Number of spaces to use for each step of (auto)indent
+vim.opt.shiftwidth = 4
+
+-- When using <Tab> in insert mode, use this many spaces
+vim.opt.softtabstop = 4
+vim.opt.wrap = false
+
+-- ============================================================
+--  PACKER: PLUGINS
+-- ============================================================
+require("packer").startup(function(use)
+    use "wbthomason/packer.nvim"
+
+    -- Kanagawa colorscheme
+    use "rebelot/kanagawa.nvim"
+
+    -- Treesitter
+    use {
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate",
+    }
+
+    -- nvim-tree (file explorer)
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional, for file icons
+        },
+        config = function()
+            require("nvim-tree").setup({
+                sort_by = "case_sensitive",
+                view = {
+                    width = 30,
+                },
+                renderer = {
+                    group_empty = true,
+                },
+                filters = {
+                    dotfiles = false,
+                },
+            })
+        end
+    }
+
+    -- Telescope
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = { {'nvim-lua/plenary.nvim'} },
+        config = function()
+            local ok, telescope = pcall(require, "telescope")
+            if not ok then return end
+            telescope.setup{
+                defaults = {
+                    prompt_prefix = "🔍 ",
+                    selection_caret = "➤ ",
+                    path_display = {"smart"},
+                    mappings = {
+                        i = {
+                            ["<C-j>"] = "move_selection_next",
+                            ["<C-k>"] = "move_selection_previous",
+                            ["<C-q>"] = "send_to_qflist + open_qflist"
+                        },
+                    },
+                }
+            }
+            -- Load FZF extension
+            pcall(telescope.load_extension, 'fzf')
+        end
+    }
+
+    -- Telescope FZF Native (faster sorting)
+    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make'}
+
+    if packer_bootstrap then
+        require("packer").sync()
+    end
+end)
+
+-- ============================================================
+--  TELESCOPES KEYBINDINGS
+-- ============================================================
+local opts = { noremap = true, silent = true }
+vim.api.nvim_set_keymap('n', '<Leader>ff', '<cmd>Telescope find_files<cr>', opts)
+vim.api.nvim_set_keymap('n', '<Leader>fg', '<cmd>Telescope live_grep<cr>', opts)
+vim.api.nvim_set_keymap('n', '<Leader>fb', '<cmd>Telescope buffers<cr>', opts)
+vim.api.nvim_set_keymap('n', '<Leader>fh', '<cmd>Telescope help_tags<cr>', opts)
+
+-- Nvim-tree keybindings
+vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>o', ':NvimTreeFocus<CR>', { noremap = true, silent = true })
+
+-- ============================================================
+--  KANAGAWA CONFIG
+-- ============================================================
+require("kanagawa").setup({
+    theme = "wave",  -- wave | dragon | lotus
+    commentStyle = { italic = true },
+    keywordStyle = { italic = true },
+})
+vim.cmd("colorscheme kanagawa")
+
+-- ============================================================
+--  TREESITTER CONFIG
+-- ============================================================
+require("nvim-treesitter.configs").setup({
+    ensure_installed = {
+        "lua", "vim", "vimdoc",
+        "python", "javascript", "html", "css", "json",
+    },
+    highlight = { enable = true },
+    indent = { enable = true },
+})
---
+++ nvim/plugin/packer_compiled.lua
@@ -0,0 +1,144 @@
+-- Automatically generated packer.nvim plugin loader code
+
+if vim.api.nvim_call_function('has', {'nvim-0.5'}) ~= 1 then
+  vim.api.nvim_command('echohl WarningMsg | echom "Invalid Neovim version for packer.nvim! | echohl None"')
+  return
+end
+
+vim.api.nvim_command('packadd packer.nvim')
+
+local no_errors, error_msg = pcall(function()
+
+_G._packer = _G._packer or {}
+_G._packer.inside_compile = true
+
+local time
+local profile_info
+local should_profile = false
+if should_profile then
+  local hrtime = vim.loop.hrtime
+  profile_info = {}
+  time = function(chunk, start)
+    if start then
+      profile_info[chunk] = hrtime()
+    else
+      profile_info[chunk] = (hrtime() - profile_info[chunk]) / 1e6
+    end
+  end
+else
+  time = function(chunk, start) end
+end
+
+local function save_profiles(threshold)
+  local sorted_times = {}
+  for chunk_name, time_taken in pairs(profile_info) do
+    sorted_times[#sorted_times + 1] = {chunk_name, time_taken}
+  end
+  table.sort(sorted_times, function(a, b) return a[2] > b[2] end)
+  local results = {}
+  for i, elem in ipairs(sorted_times) do
+    if not threshold or threshold and elem[2] > threshold then
+      results[i] = elem[1] .. ' took ' .. elem[2] .. 'ms'
+    end
+  end
+  if threshold then
+    table.insert(results, '(Only showing plugins that took longer than ' .. threshold .. ' ms ' .. 'to load)')
+  end
+
+  _G._packer.profile_output = results
+end
+
+time([[Luarocks path setup]], true)
+local package_path_str = "/Users/n45371/.cache/nvim/packer_hererocks/2.1.1764270282/share/lua/5.1/?.lua;/Users/n45371/.cache/nvim/packer_hererocks/2.1.1764270282/share/lua/5.1/?/init.lua;/Users/n45371/.cache/nvim/packer_hererocks/2.1.1764270282/lib/luarocks/rocks-5.1/?.lua;/Users/n45371/.cache/nvim/packer_hererocks/2.1.1764270282/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/n45371/.cache/nvim/packer_hererocks/2.1.1764270282/lib/lua/5.1/?.so"
+if not string.find(package.path, package_path_str, 1, true) then
+  package.path = package.path .. ';' .. package_path_str
+end
+
+if not string.find(package.cpath, install_cpath_pattern, 1, true) then
+  package.cpath = package.cpath .. ';' .. install_cpath_pattern
+end
+
+time([[Luarocks path setup]], false)
+time([[try_loadstring definition]], true)
+local function try_loadstring(s, component, name)
+  local success, result = pcall(loadstring(s), name, _G.packer_plugins[name])
+  if not success then
+    vim.schedule(function()
+      vim.api.nvim_notify('packer.nvim: Error running ' .. component .. ' for ' .. name .. ': ' .. result, vim.log.levels.ERROR, {})
+    end)
+  end
+  return result
+end
+
+time([[try_loadstring definition]], false)
+time([[Defining packer_plugins]], true)
+_G.packer_plugins = {
+  ["kanagawa.nvim"] = {
+    loaded = true,
+    path = "/Users/n45371/.local/share/nvim/site/pack/packer/start/kanagawa.nvim",
+    url = "https://github.com/rebelot/kanagawa.nvim"
+  },
+  ["nvim-tree.lua"] = {
+    config = { "\27LJ\2\nƒ\1\0\0\4\0\n\0\r6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\4\0=\3\5\0025\3\6\0=\3\a\0025\3\b\0=\3\t\2B\0\2\1K\0\1\0\ffilters\1\0\1\rdotfiles\1\rrenderer\1\0\1\16group_empty\2\tview\1\0\1\nwidth\3\30\1\0\4\rrenderer\0\tview\0\ffilters\0\fsort_by\19case_sensitive\nsetup\14nvim-tree\frequire\0" },
+    loaded = true,
+    path = "/Users/n45371/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
+    url = "https://github.com/nvim-tree/nvim-tree.lua"
+  },
+  ["nvim-treesitter"] = {
+    loaded = true,
+    path = "/Users/n45371/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter"
+  },
+  ["nvim-web-devicons"] = {
+    loaded = true,
+    path = "/Users/n45371/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    url = "https://github.com/nvim-tree/nvim-web-devicons"
+  },
+  ["packer.nvim"] = {
+    loaded = true,
+    path = "/Users/n45371/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    url = "https://github.com/wbthomason/packer.nvim"
+  },
+  ["plenary.nvim"] = {
+    loaded = true,
+    path = "/Users/n45371/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    url = "https://github.com/nvim-lua/plenary.nvim"
+  },
+  ["telescope-fzf-native.nvim"] = {
+    loaded = true,
+    path = "/Users/n45371/.local/share/nvim/site/pack/packer/start/telescope-fzf-native.nvim",
+    url = "https://github.com/nvim-telescope/telescope-fzf-native.nvim"
+  },
+  ["telescope.nvim"] = {
+    config = { "\27LJ\2\nÙ\2\0\0\b\0\15\0\0236\0\0\0006\2\1\0'\3\2\0B\0\3\3\14\0\0\0X\2\1ÄK\0\1\0009\2\3\0015\4\v\0005\5\4\0005\6\5\0=\6\6\0055\6\b\0005\a\a\0=\a\t\6=\6\n\5=\5\f\4B\2\2\0016\2\0\0009\4\r\1'\5\14\0B\2\3\1K\0\1\0\bfzf\19load_extension\rdefaults\1\0\1\rdefaults\0\rmappings\6i\1\0\1\6i\0\1\0\3\n<C-j>\24move_selection_next\n<C-q>!send_to_qflist + open_qflist\n<C-k>\28move_selection_previous\17path_display\1\2\0\0\nsmart\1\0\4\17path_display\0\20selection_caret\t‚û§ \rmappings\0\18prompt_prefix\nüîç \nsetup\14telescope\frequire\npcall\0" },
+    loaded = true,
+    path = "/Users/n45371/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    url = "https://github.com/nvim-telescope/telescope.nvim"
+  }
+}
+
+time([[Defining packer_plugins]], false)
+-- Config for: nvim-tree.lua
+time([[Config for nvim-tree.lua]], true)
+try_loadstring("\27LJ\2\nƒ\1\0\0\4\0\n\0\r6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\4\0=\3\5\0025\3\6\0=\3\a\0025\3\b\0=\3\t\2B\0\2\1K\0\1\0\ffilters\1\0\1\rdotfiles\1\rrenderer\1\0\1\16group_empty\2\tview\1\0\1\nwidth\3\30\1\0\4\rrenderer\0\tview\0\ffilters\0\fsort_by\19case_sensitive\nsetup\14nvim-tree\frequire\0", "config", "nvim-tree.lua")
+time([[Config for nvim-tree.lua]], false)
+-- Config for: telescope.nvim
+time([[Config for telescope.nvim]], true)
+try_loadstring("\27LJ\2\nÙ\2\0\0\b\0\15\0\0236\0\0\0006\2\1\0'\3\2\0B\0\3\3\14\0\0\0X\2\1ÄK\0\1\0009\2\3\0015\4\v\0005\5\4\0005\6\5\0=\6\6\0055\6\b\0005\a\a\0=\a\t\6=\6\n\5=\5\f\4B\2\2\0016\2\0\0009\4\r\1'\5\14\0B\2\3\1K\0\1\0\bfzf\19load_extension\rdefaults\1\0\1\rdefaults\0\rmappings\6i\1\0\1\6i\0\1\0\3\n<C-j>\24move_selection_next\n<C-q>!send_to_qflist + open_qflist\n<C-k>\28move_selection_previous\17path_display\1\2\0\0\nsmart\1\0\4\17path_display\0\20selection_caret\t‚û§ \rmappings\0\18prompt_prefix\nüîç \nsetup\14telescope\frequire\npcall\0", "config", "telescope.nvim")
+time([[Config for telescope.nvim]], false)
+
+_G._packer.inside_compile = false
+if _G._packer.needs_bufread == true then
+  vim.cmd("doautocmd BufRead")
+end
+_G._packer.needs_bufread = false
+
+if should_profile then save_profiles() end
+
+end)
+
+if not no_errors then
+  error_msg = error_msg:gsub('"', '\\"')
+  vim.api.nvim_command('echohl ErrorMsg | echom "Error in packer_compiled: '..error_msg..'" | echom "Please check your config for correctness" | echohl None')
+end
